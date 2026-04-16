0c53ffc1f8a55b89226a74d64a9341a4a4979942
diegomarln <josue.marin.lopezd@gmail.com>
2026-04-16 11:01:03 -0600

    refactor: update ReservationResponse structure and add ReservationMapper for entity conversion

--- DTOs/Responses/ReservationResponse.cs
+++ DTOs/Responses/ReservationResponse.cs
@@ -4,7 +4,7 @@ public class ReservationResponse
 {
     public Guid Id { get; set; }
     public Guid ClassroomId { get; set; }
-    public string ReservedBy { get; set; } = string.Empty;
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }     
+    public TimeOnly EndTime { get; set; }
 }
